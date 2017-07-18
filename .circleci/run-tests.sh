db5771d11237f96320150353faf5dc4ff642a2e6
Tim Kye <Timothy.Kye@nike.com>
2017-07-18 12:40:09 -0700

    make test:ci the cannonical script

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# make reports directory
-mkdir -p reports
-
 # run tests and save as report
-npm run test:ci -s | tee reports/ava.xml; test ${PIPESTATUS[1]}
-
-# generate the html test coverage
-npm run report
+npm run test:ci
