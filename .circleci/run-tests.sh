428de8c9358bb78cf5d7dabbe1147611e71ce3ce
Tim Kye <Timothy.Kye@nike.com>
2017-07-18 09:54:39 -0700

    store report

--- .circleci/run-tests.sh
+++ .circleci/run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
+
+# make reports directory
 mkdir -p reports
 
+# run tests and save as report
 npm run test:ci | tee reports/ava.xml; test ${PIPESTATUS[1]}
+
+# generate the html test coverage
 npm run report
